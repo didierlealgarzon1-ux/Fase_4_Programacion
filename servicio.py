d774d59ff89c0ae111c4a2b85a8ffc8059b5f6f3
didierlealgarzon1-ux <didierlealgarzon1@gmail.com>
2026-05-12 21:39:36 -0500

    Update servicio.py

--- servicio.py
+++ servicio.py
@@ -1,1 +1,34 @@
+from abc import ABC, abstractmethod
 
+
+class Servicio(ABC):
+
+    def __init__(self, nombre, precio):
+
+        if precio <= 0:
+            raise ValueError("El precio debe ser mayor a cero")
+
+        self.nombre = nombre
+        self.precio = precio
+
+    @abstractmethod
+    def calcular_costo(self, tiempo):
+        pass
+
+
+class ReservaSala(Servicio):
+
+    def calcular_costo(self, horas):
+        return self.precio * horas
+
+
+class AlquilerEquipo(Servicio):
+
+    def calcular_costo(self, dias):
+        return self.precio * dias
+
+
+class Asesoria(Servicio):
+
+    def calcular_costo(self, horas):
+        return self.precio * horas
